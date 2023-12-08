96087ab86b48d0e9c32e050dbbc5a41e2be65faf
Ahmed Abbas <115493725+A-3bbas@users.noreply.github.com>
2023-12-08 13:37:02 +0200

    Delete deployment directory

--- deployment/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.toggle-button').forEach(button => {
-    button.addEventListener('click', () => {
-      button.classList.toggle('rotate-up');
-        button.classList.toggle('rotate-down');
-        const codeBlock = button.nextElementSibling;
-        codeBlock.classList.toggle('hidden');
-    });
-});
